68d3c20e6008c632f3fb74a8988e5e93ea5eb5c8
Monique <moniquevz@gmail.com>
2020-06-27 16:03:38 +0200

    dev update

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,12 +5,13 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 const { check, validationResult } = require("express-validator");
+const normalize = require("normalize-url");
 
 const User = require("../../models/User");
-//@ route   POST api/users
-//@desc     Register User
-// @access  Public
 
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
 router.post(
   "/",
   [
@@ -30,20 +31,22 @@ router.post(
     const { name, email, password } = req.body;
 
     try {
-      // See if user exists
       let user = await User.findOne({ email });
+
       if (user) {
         return res
           .status(400)
           .json({ errors: [{ msg: "User already exists" }] });
       }
 
-      // Get users gravatar
-      const avatar = gravatar.url(email, {
-        s: "200",
-        r: "pg",
-        d: "mm",
-      });
+      const avatar = normalize(
+        gravatar.url(email, {
+          s: "200",
+          r: "pg",
+          d: "mm",
+        }),
+        { forceHttps: true }
+      );
 
       user = new User({
         name,
@@ -52,14 +55,12 @@ router.post(
         password,
       });
 
-      //Encrypt password
       const salt = await bcrypt.genSalt(10);
 
-      user.Password = await bcrypt.hash(password, salt);
+      user.password = await bcrypt.hash(password, salt);
 
       await user.save();
 
-      //Return jsonwebtoken
       const payload = {
         user: {
           id: user.id,
@@ -69,7 +70,7 @@ router.post(
       jwt.sign(
         payload,
         config.get("jwtSecret"),
-        { expiresIn: 36000000 },
+        { expiresIn: "5 days" },
         (err, token) => {
           if (err) throw err;
           res.json({ token });
